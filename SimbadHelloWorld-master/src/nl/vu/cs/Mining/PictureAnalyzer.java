7b88f526e035af8eadb069268672b85217223365
H3x <14954889+H3xFiles@users.noreply.github.com>
2019-03-07 17:05:56 +0100

    Update PictureAnalyzer.java

--- SimbadHelloWorld-master/src/nl/vu/cs/Mining/PictureAnalyzer.java
+++ SimbadHelloWorld-master/src/nl/vu/cs/Mining/PictureAnalyzer.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 public class PictureAnalyzer {
-
+//This function take a bufferImage and serialize it into an array of byte. 
 	public void AnalyzePictures(BufferedImage buffImage) {
 		byte[] pixels = ((DataBufferByte) buffImage.getRaster().getDataBuffer()).getData();
 		for(int i = 0; i<pixels.length; i+=1) {
